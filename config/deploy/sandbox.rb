0a603b12353d031ac40e54a389c9f3b2bae896e5
宫贺 <gonghe@creatingev.com>
2018-01-02 15:45:24 +0800

    add sanbox

---
+++ config/deploy/sandbox.rb
@@ -0,0 +1,8 @@
+role :app, %w{ali-sandbox1}
+role :web, %w{ali-sandbox1}
+role :db, %w{ali-sandbox1}
+
+set :stage        ,   :sandbox
+set :rails_env    ,   :sandbox
+set :branch       ,   :master
+set :deploy_to    ,   "/var/www/#{fetch(:application)}/#{fetch(:stage)}"
